95bacb71f83aef376b98188e8e3dfcae966e33d0
Ivraj Seerha <iseerha@college.harvard.edu>
2019-06-03 22:33:47 -0400

    Adds under construction page

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Ivraj's Portfolio`,
+    description: `This is Ivraj's Portfolio Page.`,
+    author: `@ivraj`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
